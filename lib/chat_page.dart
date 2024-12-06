481460e84009728a0b8082dca74e36a1aa5df561
moab5 <moabadejumat@gmail.com>
2024-12-06 18:11:35 +0400

    - Load mock data for chat from JSON file - Add width constraint to chat bubbles

--- lib/chat_page.dart
+++ lib/chat_page.dart
@@ -1,10 +1,40 @@
+import 'dart:convert';
+
 import 'package:chat_app/Widgets/chat_bubble.dart';
 import 'package:chat_app/Widgets/chat_input.dart';
 import 'package:chat_app/models/chat_message_entity.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   ChatPage({super.key});
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  List<ChatMessageEntity> _messages = [];
+
+  _loadInitialMessages() async {
+    final response = await rootBundle.loadString('assets/mock_messages.json');
+
+    final List<dynamic> decodedList = jsonDecode(response) as List;
+
+    final List<ChatMessageEntity> _chatMessages = decodedList.map((listItem) {
+      return ChatMessageEntity.fromJson(listItem);
+    }).toList();
+
+    setState(() {
+      _messages = _chatMessages;
+    });
+  }
+
+  @override
+  void initState() {
+    _loadInitialMessages();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -14,8 +44,10 @@ class ChatPage extends StatelessWidget {
         backgroundColor: Colors.transparent,
         elevation: 0,
         title: Center(
-          child: Text("Hi $username",
-            style: const TextStyle(fontWeight: FontWeight.bold),),
+          child: Text(
+            "Hi $username",
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
         ),
         actions: [
           IconButton(
@@ -28,28 +60,15 @@ class ChatPage extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: 5,
-              itemBuilder: (context, index) {
-                return ChatBubble(
-                    alignment: index % 2 == 0 ?
-                      Alignment.centerRight : Alignment.centerLeft,
-                    entity: index % 2 == 0 ?
-                      ChatMessageEntity(
-                          text: "Hi, I'm Moab.",
-                          id: "001",
-                          createdAt: DateTime.now().second,
-                          author: Author(userName: username),
-                          imageUrl: "assets/chat_bubble.jpeg"
-                      ) : ChatMessageEntity(
-                      text: "Hello $username.",
-                      id: "002",
-                      createdAt: DateTime.now().second,
-                      author: Author(userName: "ChatBot"),
-                    ));
-              }
-            ),
-          ),
+              child: ListView.builder(
+                  itemCount: _messages.length,
+                  itemBuilder: (context, index) {
+                    return ChatBubble(
+                        alignment: _messages[index].author.userName == 'Moab'
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,
+                        entity: _messages[index]);
+                  })),
           ChatInput()
         ],
       ),
